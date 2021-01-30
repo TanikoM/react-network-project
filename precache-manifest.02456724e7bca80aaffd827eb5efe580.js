660fb80e82a142b2ffb64724d19b9b0e197dcc99
Horizon903 <taniko2425@gmail.com>
2021-01-31 00:10:41 +0200

    Updates

---
+++ precache-manifest.02456724e7bca80aaffd827eb5efe580.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "8eb6aef28a44bca314c7",
+    "url": "/samurai-network/static/css/main.92bf502d.chunk.css"
+  },
+  {
+    "revision": "8eb6aef28a44bca314c7",
+    "url": "/samurai-network/static/js/main.8eb6aef2.chunk.js"
+  },
+  {
+    "revision": "26f591d21070e0f8beda",
+    "url": "/samurai-network/static/css/1.dd901179.chunk.css"
+  },
+  {
+    "revision": "26f591d21070e0f8beda",
+    "url": "/samurai-network/static/js/1.26f591d2.chunk.js"
+  },
+  {
+    "revision": "a449bd6f1dcc045d578f",
+    "url": "/samurai-network/static/css/2.aebed313.chunk.css"
+  },
+  {
+    "revision": "a449bd6f1dcc045d578f",
+    "url": "/samurai-network/static/js/2.a449bd6f.chunk.js"
+  },
+  {
+    "revision": "d81c966f836397a21043",
+    "url": "/samurai-network/static/js/3.d81c966f.chunk.js"
+  },
+  {
+    "revision": "e85e105d8303319683a0",
+    "url": "/samurai-network/static/js/runtime~main.e85e105d.js"
+  },
+  {
+    "revision": "08e6ccaf214ba8d9a7f4f9d0294df585",
+    "url": "/samurai-network/static/media/user.08e6ccaf.png"
+  },
+  {
+    "revision": "900ec5a6df455adb47083844048c0a97",
+    "url": "/samurai-network/static/media/preloader.900ec5a6.svg"
+  },
+  {
+    "revision": "9f35e9ed6f09ad191ab479d55374ab45",
+    "url": "/samurai-network/index.html"
+  }
+];
